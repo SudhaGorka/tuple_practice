6fd1e2d8091651b33963be68135eca3bc930541d
Sudha Gorka <100294985+SudhaGorka@users.noreply.github.com>
2024-01-04 14:41:02 +0530

    Add files via upload

---
+++ Swap_Tuples.py
@@ -0,0 +1,6 @@
+#Swap two tuple in python
+
+Tuple1 = eval(input("Enter first tuple: "))
+Tuple2 = eval(input("Enter second tuple: "))
+Tuple1,Tuple2=Tuple2,Tuple1
+print("After Swapping", "\n"," First Tuple is: ",Tuple1,"\n","& Second Tuple is: ",Tuple2)
